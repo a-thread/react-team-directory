773d9aed70b115bb4c35b57d8089e9da80ffac08
a-thread <aiden.threadgoode@gmail.com>
2020-10-30 23:27:03 -0400

    simplifying API call

--- src/utils/API.tsx
+++ src/utils/API.tsx
@@ -1,21 +1,9 @@
 import axios from "axios";
 
-const API = () => {
-    axios
-        .get("https://randomuser.me/api/?results=200&nat=us")
-        .then(res => {
-            const users = res.data;
-            console.log(users);
-            // const results = users.map(user => {
-            //     return {
-            //         login: user.login,
-            //         image: user.avatar_url,
-            //         language: language
-            //     };
-            // });
-            // resolve(results);
-        })
-        .catch(err => console.log(err));
-}; 
+const API = {
+    getEmployees: function () {
+        return axios.get("https://randomuser.me/api/?results=200&nat=us")
+    }
+}
 
 export default API;
