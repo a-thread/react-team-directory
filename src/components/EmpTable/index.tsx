cd59e10b235ec03e7e560a0beca4b0a948ee7123
a-thread <aiden.threadgoode@gmail.com>
2020-11-01 21:00:14 -0500

    adding TableSprtLabels

--- src/components/EmpTable/index.tsx
+++ src/components/EmpTable/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -6,9 +6,25 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Paper from '@material-ui/core/Paper';
 
-function EmpTable(props: { results: any[]; }) {
+function EmpTable(props: { results: any[]; orderBy: string; order: string; handleSort: (arg0: string, arg1: any) => void; }) {
+
+    const empArr = props.results;
+    const [sortedField, setSortedField] = useState(null);
+    const sortedArr = [...empArr];
+
+    if (sortedField)
+        sortedArr.sort((a, b) => {
+            if (a.firstName < b.firstName) {
+                return -1;
+            }
+            if (a.firstName > b.firstName) {
+                return 1;
+            }
+            return 0;
+        });
 
     const useStyles = makeStyles({
         table: {
@@ -36,19 +52,31 @@ function EmpTable(props: { results: any[]; }) {
             <Table className={classes.table} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell align="center">ID</TableCell>
-                        <TableCell align="center">Photo</TableCell>
-                        <TableCell align="center">First Name</TableCell>
-                        <TableCell align="center">Last Name</TableCell>
-                        <TableCell align="center">Email</TableCell>
-                        <TableCell align="center">Phone Number</TableCell>
+                        <TableCell align="center">ID
+                        <TableSortLabel active={props.orderBy === "firstName"} onClick={() => props.handleSort("id", props.order)}></TableSortLabel>
+                        </TableCell>
+                        <TableCell align="center">Photo
+                        <TableSortLabel active={props.orderBy === "photo"} onClick={() => props.handleSort("photo", props.order)}></TableSortLabel>
+                        </TableCell>
+                        <TableCell align="center">First Name
+                        <TableSortLabel active={props.orderBy === "firstName"} onClick={() => props.handleSort("firstName", props.order)}></TableSortLabel>
+                        </TableCell>
+                        <TableCell align="center">Last Name
+                        <TableSortLabel active={props.orderBy === "lastName"} onClick={() => props.handleSort("lastName", props.order)}></TableSortLabel>
+                        </TableCell>
+                        <TableCell align="center">Email
+                        <TableSortLabel active={props.orderBy === "email"} onClick={() => props.handleSort("email", props.order)}></TableSortLabel>
+                        </TableCell>
+                        <TableCell align="center">Phone Number
+                        <TableSortLabel active={props.orderBy === "phone"} onClick={() => props.handleSort("cell", props.order)}></TableSortLabel>
+                        </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {rows}
                 </TableBody>
             </Table>
-        </TableContainer>
+        </TableContainer >
     )
 };
 
