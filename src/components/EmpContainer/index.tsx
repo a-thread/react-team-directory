86e0c4e7483af8f6cd7ad272348401f62d7dcad0
a-thread <aiden.threadgoode@gmail.com>
2020-11-01 21:52:40 -0500

    restructuring

--- src/components/EmpContainer/index.tsx
+++ src/components/EmpContainer/index.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import EmpTable from "../EmpTable";
+import SearchBar from "../SearchBar";
 import API from "../../utils/API";
 import sortEmp from "../../utils/sortEmp";
+import "./style.css";
 
 class EmpContainer extends Component {
 
@@ -16,16 +18,23 @@ class EmpContainer extends Component {
     componentDidMount() {
         API.getEmployees()
             .then((res) => {
-                let empData = res.data.results.map((emp: { id: { value: any; }; picture: { medium: any; }; name: { first: any; last: any; }; email: any; cell: any; }) => {
-                    return {
-                        id: emp.id.value,
-                        picture: emp.picture.medium,
-                        firstName: emp.name.first,
-                        lastName: emp.name.last,
-                        email: emp.email,
-                        cell: emp.cell
-                    }
-                })
+                let empData = res.data.results.map(
+                    (emp:
+                        {
+                            id: { value: string; };
+                            picture: { medium: string; };
+                            name: { first: string; last: string; };
+                            email: string; cell: string;
+                        }) => {
+                        return {
+                            id: emp.id.value,
+                            picture: emp.picture.medium,
+                            firstName: emp.name.first,
+                            lastName: emp.name.last,
+                            email: emp.email,
+                            cell: emp.cell
+                        }
+                    })
                 this.setState({
                     empArr: empData,
                     sortedArr: empData
@@ -45,9 +54,37 @@ class EmpContainer extends Component {
         });
     }
 
+    handleInput = (event: { target: { value: any; name: any; }; }) => {
+        let value = event.target.value;
+        const name = event.target.name;
+
+        this.setState({
+            [name]: value
+        }, () => {
+            if (this.state.search) {
+                let filteredArr = sortEmp.searchEmp(this.state.search, this.state.sortedArr);
+                this.setState({
+                    empArr: filteredArr,
+                    orderBy: ""
+                })
+            } else {
+                this.setState({
+                    empArr: this.state.sortedArr,
+                    orderBy: ""
+                })
+            }
+        });
+    };
+
     render() {
         return (
-            <EmpTable results={this.state.empArr} handleSort={this.handleSort} orderBy={this.state.orderBy} order={this.state.order}  />)
+            <>
+                <div className="searchContainer">
+                    <SearchBar handleInput={this.handleInput} />
+                </div>
+                <EmpTable results={this.state.empArr} handleSort={this.handleSort} orderBy={this.state.orderBy} order={this.state.order} />
+            </>
+        )
     }
 }
 
